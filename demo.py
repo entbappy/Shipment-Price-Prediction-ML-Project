b26bc3647feda64eafe2e42f14d708e2f65d7587
entbappy <entbappy73@gmail.com>
2026-05-14 22:07:46 +0600

    utility helper functions added

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@
 from shipment.logger import logging
 from shipment.exception import shippingException
 import sys
+from shipment.utils.main_utils import MainUtils
 
+obj = MainUtils()
 
-try:
-    a = 1 / 0
-except Exception as e:
-    raise shippingException(e, sys) from e
+data = obj.read_yaml_file("config/model.yaml")
+print(data)
