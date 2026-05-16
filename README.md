13de4bd4a2bd404094cfc2c00569332feeb99688
entbappy <entbappy73@gmail.com>
2026-05-16 21:41:05 +0600

    data validation added

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ Step 3. Install the requirements
 ```
 pip install -r requirements.txt
 ```
+
+
+# Workflow
+
+1. Constants
+2. config_entity
+3. artifacts_entity
+4. Components
+5. pipeline
+6. main.py
--- shipment/entity/artifacts_entity.py
+++ shipment/entity/artifacts_entity.py
@@ -5,3 +5,10 @@ from dataclasses import dataclass
 class DataIngestionArtifacts:
     train_data_file_path: str
     test_data_file_path: str
+
+
+
+@dataclass
+class DataValidationArtifacts:
+    data_drift_file_path: str
+    validation_status: bool
